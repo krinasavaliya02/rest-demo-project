9ba15ada83b5bf667f2cb57ac28fcf63ac72250b
krinasavaliya02 <krina211@gmail.com>
2026-05-22 15:31:00 +0530

    add mixins

--- my_app/views.py
+++ my_app/views.py
@@ -1,12 +1,12 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 from .models import Student
 from .serializers import StudentSerializer
 from .pagination import MyPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 
-class StudentList(generics.ListCreateAPIView):
-
+class StudentList(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+   
     queryset = Student.objects.all().order_by('id')
 
     serializer_class = StudentSerializer
@@ -16,9 +16,24 @@ class StudentList(generics.ListCreateAPIView):
     filterset_fields = ['name', 'city']
     ordering_fields = ['name']
 
+    def get(self, request):
+            return self.list(request)   
+    
+    def post(self, request):
+            return self.create(request)
 
-class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
+
+class StudentDetail(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
 
     queryset = Student.objects.all().order_by('id')
 
     serializer_class = StudentSerializer
+
+    def get(self, request, pk):
+        return self.retrieve(request, pk)       
+    
+    def put(self, request, pk):
+        return self.update(request, pk)
+    
+    def delete(self, request, pk):
+        return self.destroy(request, pk)
